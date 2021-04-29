89dd27f5826b4437dee9f27a4c10985f8d7d95a8
William Angulo <wmavalle@gmail.com>
2021-04-29 12:11:35 -0500

    "brute-force in progress"

---
+++ bruteforce.py
@@ -0,0 +1,21 @@
+##Define districts
+#BRUTE FORCE
+#TOURS CALCULATED= (N-1)! 
+import numpy as np
+
+routes = []
+
+#def cal_distance(){
+
+#}
+
+#def 
+
+#def build_path(node,district,path){
+
+#}
+
+if __name__== '__main__':
+ file  = open("bru.csv","r")
+ linea1= file.read()
+ print(linea1)
